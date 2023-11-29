3e04a2b358f83ed194a75876dcc4b29246cffb1f
Partha Majumder <parthamajumdet15@gmail.com>
2023-11-30 03:36:01 +0530

    Initial commit hi

---
+++ src/first_class.java
@@ -0,0 +1,11 @@
+public class first_class {
+    public static void main(String[] args)
+    {
+        int a,b,sum;
+        a = 10;
+        b = 20;
+        sum = (a+b);
+
+        System.out.println("output is "+sum);
+    }
+}
